a7b6d96dfb6223ca227c4398c71129d5773c3d3f
dgroh <daniel.groh@iti-software.de>
2018-02-15 17:24:39 +0100

    removed clone from dropped element

--- src/app/droppable.directive.ts
+++ src/app/droppable.directive.ts
@@ -13,17 +13,14 @@ export class DroppableDirective {
   onDrop(event) {
     const dropzone = event.target;
     const droppedElementId = event.dataTransfer.getData('text');
-    const droppedElement = document
-      .getElementById(droppedElementId)
-      .cloneNode(true) as any;
-
-    droppedElement.removeAttribute('id');
+    const droppedElement = document.getElementById(droppedElementId) as any;
 
     dropzone.appendChild(droppedElement);
 
     const svgPoint = this.svgService.getSVGPoint(event, droppedElement);
 
     droppedElement.setAttribute('draggable', true);
+
     droppedElement.setAttribute('cx', svgPoint.x);
     droppedElement.setAttribute('cy', svgPoint.y);
   }
